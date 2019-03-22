d4f70c5bc3fd0f8dac9b54595b164f678e3f6ea4
Matthew Hosking <hosking.m@husky.neu.edu>
2019-03-22 03:19:22 -0400

    Matt - added stub classes for Mobs

--- src/World/Mobs/Mob.java
+++ src/World/Mobs/Mob.java
@@ -1,13 +1,17 @@
 package World.Mobs;
 
+// The actual representation of a mob that exists in the level somewhere
+// THIS CLASS IS A STUB
 public class Mob {
   private int mobPrototypeID;
   private int x;
   private int y;
+  private float health;
 
   public Mob(int x, int y, MobPrototype mobPrototype) {
     this.mobPrototypeID = mobPrototype.ID;
     this.x = x;
     this.y = y;
+    this.health = mobPrototype.getMaxHealth();
   }
 }
--- src/World/Mobs/MobPrototype.java
+++ src/World/Mobs/MobPrototype.java
@@ -2,12 +2,21 @@ package World.Mobs;
 
 // We use these prototypes to create mobs of this type.
 // See Mob class for more info on process.
+
+// THIS CLASS IS A STUB
 public class MobPrototype {
   private static int ID_COUNTER = 0;
 
   public final int ID;
 
-  public MobPrototype() {
+  private float maxHealth;
+
+  public MobPrototype(float health) {
     this.ID = ID_COUNTER++;
+    this.maxHealth = health;
+  }
+
+  public float getMaxHealth() {
+    return maxHealth;
   }
 }
