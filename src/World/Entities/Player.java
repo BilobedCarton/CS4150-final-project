8df2ec3329e0b0ca06cae1a05dd9acf12a70c1a3
Matthew Hosking <hosking.m@husky.neu.edu>
2019-04-09 13:00:19 -0400

    temp

--- src/World/Entities/Player.java
+++ src/World/Entities/Player.java
@@ -1,5 +1,10 @@
 package World.Entities;
 
+import World.WorldController;
+import processing.core.PApplet;
+
+import java.awt.*;
+
 public class Player extends AbstractEntity {
   public Player(int x, int y) {
     super(x, y, 100);
@@ -11,6 +16,14 @@ public class Player extends AbstractEntity {
 
   @Override
   public void draw() {
-
+    PApplet sketch = WorldController._instance.sketch;
+    int cellDimension = WorldController._instance.cellDimension;
+    sketch.stroke(Color.RED.getRed(), Color.RED.getGreen(), Color.RED.getBlue());
+    sketch.fill(Color.RED.getRed(), Color.RED.getGreen(), Color.RED.getBlue());
+    sketch.ellipse(
+            this.getX() * cellDimension + 1,
+            this.getY() * cellDimension + 1,
+            cellDimension - 1,
+            cellDimension - 1);
   }
 }
